2270602a453717bbad7509c728bea668e9f4bce6
ashishshres <ashishshr27@gmail.com>
2024-09-19 11:49:48 +0545

    add header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./partials/Navbar";
 import Searchbar from "./partials/Searchbar";
+import axios from "../utils/Axios";
+import Header from "./partials/Header";
 
 const Home = () => {
     document.title = "Fusion | Home";
-    return (
+    const [trendingBanner, setTrendingBanner] = useState(null);
+
+    const getTrendingBanner = async () => {
+        try {
+            const { data } = await axios.get(
+                `/trending/all/day?language=en-US`
+            );
+            const chooseBanner =
+                data.results[(Math.random() * data.results.length).toFixed()];
+
+            console.log(chooseBanner);
+            setTrendingBanner(chooseBanner);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        !trendingBanner && getTrendingBanner();
+    }, []);
+
+    return trendingBanner ? (
         <div className="w-full">
             <Navbar />
-            <Searchbar />
+            {/* <Searchbar /> */}
+            <Header data={trendingBanner} />
         </div>
+    ) : (
+        <h1>Loading</h1>
     );
 };
 
