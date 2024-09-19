d205d3e1686a32536f51ed2e4693203978a61232
ashishshres <ashishshr27@gmail.com>
2024-09-19 10:43:34 +0545

    add classname

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -5,28 +5,28 @@ const Navbar = () => {
     return (
         <div className="w-full bg-zinc-800 text-white shadow-sm px-8 py-5 flex justify-between items-center">
             <h1 className="flex items-center gap-2 text-3xl font-bold">
-                <i class="ri-movie-2-fill text-4xl text-[#1DB2F4]"></i>
+                <i className="ri-movie-2-fill text-4xl text-[#1DB2F4]"></i>
                 <span>Fusion.</span>
             </h1>
             <nav className="flex gap-3 text-lg font-semibold text-zinc-300">
                 <Link className="hover:text-[#1db2f4] hover:bg-zinc-700 px-4 py-2 rounded-md duration-300 flex gap-2">
-                    <i class="ri-fire-fill"></i>
+                    <i className="ri-fire-fill"></i>
                     <span>Trending</span>
                 </Link>
                 <Link className="hover:text-[#1db2f4] hover:bg-zinc-700 px-4 py-2 rounded-md duration-300 flex gap-2">
-                    <i class="ri-bard-fill"></i>
+                    <i className="ri-bard-fill"></i>
                     <span>Popular</span>
                 </Link>
                 <Link className="hover:text-[#1db2f4] hover:bg-zinc-700 px-4 py-2 rounded-md duration-300 flex gap-2">
-                    <i class="ri-movie-fill"></i>
+                    <i className="ri-movie-fill"></i>
                     <span>Movies</span>
                 </Link>
                 <Link className="hover:text-[#1db2f4] hover:bg-zinc-700 px-4 py-2 rounded-md duration-300 flex gap-2">
-                    <i class="ri-tv-fill"></i>
+                    <i className="ri-tv-fill"></i>
                     <span>TV Shows</span>
                 </Link>
                 <Link className="hover:text-[#1db2f4] hover:bg-zinc-700 px-4 py-2 rounded-md duration-300 flex gap-2">
-                    <i class="ri-group-fill"></i>
+                    <i className="ri-group-fill"></i>
                     <span>People</span>
                 </Link>
             </nav>
