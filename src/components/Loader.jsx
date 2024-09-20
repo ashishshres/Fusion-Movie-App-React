97fd5ddeaf5dabad87eb6a0ee4c387c019ec064b
ashishshres <ashishshr27@gmail.com>
2024-09-20 21:13:49 +0545

    add loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import loader from "../../public/loader.gif";
+
+const Loader = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-black">
+            <img src={loader} alt="" className="h-52" />
+        </div>
+    );
+};
+
+export default Loader;
