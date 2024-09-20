c91010d049f59bbd022df902d829ade32bb43def
ashishshres <ashishshr27@gmail.com>
2024-09-20 21:14:24 +0545

    fix position

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -4,13 +4,16 @@ import Searchbar from "./Searchbar";
 
 const Navbar = () => {
     return (
-        <div className="bg-zinc-800/70 text-white shadow-sm px-8 py-5 flex justify-between items-center sticky top-0 left-0 backdrop-blur-xl flex-wrap">
+        <div className="bg-zinc-800/70 text-white shadow-sm px-8 py-5 flex justify-between items-center sticky top-0 left-0 backdrop-blur-xl flex-wrap z-10">
             <h1 className="flex items-center gap-2 text-3xl font-bold">
                 <i className="ri-movie-2-fill text-3xl text-[#1DB2F4]"></i>
                 <span>Fusion.</span>
             </h1>
             <nav className="flex gap-3 text-lg font-semibold text-zinc-300">
-                <Link className="hover:text-[#1db2f4] hover:bg-zinc-700 px-3 py-2 rounded-md duration-300 flex gap-2">
+                <Link
+                    to="/trending"
+                    className="hover:text-[#1db2f4] hover:bg-zinc-700 px-3 py-2 rounded-md duration-300 flex gap-2"
+                >
                     <i className="ri-fire-fill"></i>
                     <span>Trending</span>
                 </Link>
