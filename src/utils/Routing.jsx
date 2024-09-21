de299e34c5f4b06acecf0e3233c3f50e2f141604
ashishshres <ashishshr27@gmail.com>
2024-09-21 14:17:23 +0545

    refactor: add routes for popular, movie, shows and people

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import Trending from "../components/Trending";
+import Popular from "../components/Popular";
+import Movie from "../components/Movie";
+import Show from "../components/Show";
+import People from "../components/People";
 
 const Routing = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/trending" element={<Trending />} />
+            <Route path="/popular" element={<Popular />} />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/show" element={<Show />} />
+            <Route path="/people" element={<People />} />
         </Routes>
     );
 };
