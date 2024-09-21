7705add0b764113e205807c99199812f52e4fa7d
ashishshres <ashishshr27@gmail.com>
2024-09-21 14:22:43 +0545

    refactor: add card component

--- src/components/partials/PopularCards.jsx
+++ src/components/partials/PopularCards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const PopularCards = ({ data }) => {
     return (
@@ -7,33 +8,22 @@ const PopularCards = ({ data }) => {
             <h1 className="text-zinc-200 font-bold text-2xl mb-4">Popular</h1>
             <div className="w-full h-72 flex overflow-x-auto gap-3 scrollbar-hide">
                 {data.map((popular) => (
-                    <Link
-                        style={{
-                            background: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.6), rgba(0,0,0,0.8)), url(
-                    https://image.tmdb.org/t/p/original/${
-                        popular.backdrop_path || popular.profile_path
-                    }
-                )`,
-                            backgroundPosition: "center",
-                            backgroundSize: "cover",
-                        }}
-                        key={popular.id}
-                        className="min-w-56 p-4 rounded-md flex flex-col justify-end gap-1 shadow-md hover:scale-[.98] duration-200"
-                    >
-                        <h1 className="text-xl font-bold text-zinc-200">
-                            {popular.original_title ||
-                                popular.name ||
-                                popular.original_name ||
-                                popular.title}
-                        </h1>
-                        <p className="text-zinc-400 font-medium leading-5 text-sm">
-                            {popular.overview.slice(0, 45)}...{" "}
-                            <Link className="text-[#178cbe] text-sm font-medium">
-                                more
-                            </Link>
-                        </p>
-                    </Link>
+                    <Card data={popular} />
                 ))}
+                <Link
+                    to={"/popular"}
+                    className="text-zinc-100 rounded-md text-xl font-medium h-full min-w-56 p-4 flex gap-2 items-center justify-center hover:scale-[.98] duration-200"
+                    style={{
+                        background: `linear-gradient(
+                                    rgba(23, 140, 190, 0.4),
+                                    rgba(23, 140, 190, 0.6),
+                                    rgba(23, 140, 190, 0.8)
+                                    )`,
+                    }}
+                >
+                    <i class="ri-compass-fill text-3xl"></i>
+                    <h1>Discover More</h1>
+                </Link>
             </div>
         </div>
     );
--- src/components/partials/TrendingCards.jsx
+++ src/components/partials/TrendingCards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const TrendingCards = ({ data }) => {
     return (
@@ -7,33 +8,8 @@ const TrendingCards = ({ data }) => {
             <h1 className="text-zinc-200 font-bold text-2xl mb-4">Trending</h1>
             <div className="w-full h-72 flex overflow-x-auto gap-3 scrollbar-hide">
                 {data.map((trending) => (
-                    <Link
-                        style={{
-                            background: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.6), rgba(0,0,0,0.8)), url(
-                    https://image.tmdb.org/t/p/original/${
-                        trending.backdrop_path || trending.profile_path
-                    }
-                )`,
-                            backgroundPosition: "center",
-                            backgroundSize: "cover",
-                        }}
-                        key={trending.id}
-                        className="min-w-56 p-4 rounded-md flex flex-col justify-end gap-1 shadow-md hover:scale-[.98] duration-200"
-                    >
-                        <h1 className="text-xl font-bold text-zinc-200">
-                            {trending.original_title ||
-                                trending.name ||
-                                trending.original_name ||
-                                trending.title}
-                        </h1>
-                        <p className="text-zinc-400 font-medium leading-5 text-sm">
-                            {trending.overview.slice(0, 45)}...{" "}
-                            <Link className="text-[#178cbe] text-sm font-medium">
-                                more
-                            </Link>
-                        </p>
-                    </Link>
-                ))}{" "}
+                    <Card key={trending.id} data={trending} />
+                ))}
                 <Link
                     to={"/trending"}
                     className="text-zinc-100 rounded-md text-xl font-medium h-full min-w-56 p-4 flex gap-2 items-center justify-center hover:scale-[.98] duration-200"
