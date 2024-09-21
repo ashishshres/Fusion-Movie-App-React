bc670fb6a9b6516568efd5f422f5aeefe943e732
ashishshres <ashishshr27@gmail.com>
2024-09-21 14:22:19 +0545

    add now playing, tv shows carousel

---
+++ src/components/partials/PlayingCards.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "./Card";
+
+const PlayingCards = ({ data }) => {
+    return (
+        <div className="w-full p-8 overflow-hidden">
+            <h1 className="text-zinc-200 font-bold text-2xl mb-4">
+                Now Playing
+            </h1>
+            <div className="w-full h-72 flex overflow-x-auto gap-3 scrollbar-hide">
+                {data.map((movie) => (
+                    <Card data={movie} />
+                ))}
+                <Link
+                    to={"/movie"}
+                    className="text-zinc-100 rounded-md text-xl font-medium h-full min-w-56 p-4 flex gap-2 items-center justify-center hover:scale-[.98] duration-200"
+                    style={{
+                        background: `linear-gradient(
+                                    rgba(23, 140, 190, 0.4),
+                                    rgba(23, 140, 190, 0.6),
+                                    rgba(23, 140, 190, 0.8)
+                                    )`,
+                    }}
+                >
+                    <i class="ri-compass-fill text-3xl"></i>
+                    <h1>Discover More</h1>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default PlayingCards;
